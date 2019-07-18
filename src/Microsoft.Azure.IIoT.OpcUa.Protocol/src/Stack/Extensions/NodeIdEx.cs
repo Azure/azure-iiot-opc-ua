b21515be208a67006153926227483e6d8bdf2201
Cristian Pogacean <crpogace@microsoft.com>
2019-07-18 13:44:53 +0200

    * Accept extended node Ids having nonstandard (non uri format) namespace string * allow certificates of 1024 key size * discover UA servers on the network adaptrer's gateways IP address for the edge secanrio

--- src/Microsoft.Azure.IIoT.OpcUa.Protocol/src/Stack/Extensions/NodeIdEx.cs
+++ src/Microsoft.Azure.IIoT.OpcUa.Protocol/src/Stack/Extensions/NodeIdEx.cs
@@ -256,8 +256,17 @@ namespace Opc.Ua.Extensions {
         private static object ParseNodeIdUri(string value, out string nsUri, out string srvUri) {
             // Get resource uri
             if (!Uri.TryCreate(value, UriKind.Absolute, out var uri)) {
-                // Not a absolute uri, must be default namesapce
-                nsUri = Namespaces.OpcUa;
+                
+				// Not a absolute uri, try to mitigate a potentially nonstandard namespace string
+                var identifier = value?.IndexOf('#') ?? -1;
+                if (identifier != -1){
+                    nsUri = value.Substring(0,identifier);
+                    value = value.Substring(identifier+1);
+				}
+                else{
+                    //	must be default namesapce
+                    nsUri = Namespaces.OpcUa;
+                }
             }
             else {
                 if (string.IsNullOrEmpty(uri.Fragment)) {
